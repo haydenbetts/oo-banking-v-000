7884a3e15c7cf812e8dd4574b4cf10290fccc8e6
Hayden Betts <haydenbetts@gmail.com>
2018-06-28 19:56:00 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,8 +18,9 @@ class Transfer
       receiver.balance += amount
       self.status = "complete"
     else
+      self.status = "rejected"
       return "Transaction rejected. Please check your account balance."
-    
+
   end
 
 end
